ff75d167e6742ea8fb64bb636b6ed4b104e85fab
marmoh2002 <mhabiba2002@gmail.com>
2026-01-03 12:48:00 +0200

    lab 2 task 2

--- Data Structure/Lab2/lab2-task1.cpp
+++ Data Structure/Lab2/lab2-task1.cpp
@@ -1,22 +1,43 @@
-// using stack methods implemented on top of array top of empty array is 0, write 2 functions :
+// using stack methods implemented on top of linked list top of empty array is 0, write 2 functions :
 //- Push_data : a function that push integers into a stack
 //- Pop_data : a function that retrieve data (pop) from the stack and prints "stack is empty" when there r no data in the stack
 #include <iostream>
 using namespace std;
 #define MAX 100
+struct Node
+{
+    int data;
+    Node *pnext;
+};
 class Stack
 {
-    int ar[10]; //+ve value only
-    int tos;
+    Node *pTop;
+    int count()
+    {
+        int cnt = 0;
+        Node *current = pTop;
+        while (current)
+        {
+            cnt++;
+            current = current->pnext;
+        }
+        return cnt;
+    }
 
 public:
+    Stack()
+    {
+        pTop = nullptr;
+    }
     int Push_data(int data)
     {
         int retval = 0;
-        if (tos < 10)
+        if (count() < MAX)
         {
-            ar[tos] = data;
-            tos++;
+            Node *newNode = new Node();
+            newNode->data = data;
+            newNode->pnext = pTop;
+            pTop = newNode;
             retval = 1;
         }
         return retval;
@@ -24,46 +45,42 @@ public:
     int Pop_data(void)
     {
         int retval = -1;
-        if (tos > 0)
+        if (pTop)
         {
-            tos--;
-            retval = ar[tos];
+            Node *temp = pTop;
+            retval = pTop->data;
+            pTop = pTop->pnext;
+            delete temp;
         }
         return retval;
-    }
-    Stack()
-    {
-        tos = 0;
     }
 };
 int main()
 {
-    Stack s1;
-    int data, ret;
-    cout << "Enter data to push into stack (-1 to stop): ";
-    while (true)
+    Stack stack;
+    // Push some data onto the stack
+    for (int i = 1; i <= 5; i++)
     {
-        cin >> data;
-        if (data == -1)
-            break;
-        if (s1.Push_data(data))
-            cout << data << " pushed into stack." << endl;
-        else
-            cout << "Stack overflow. Cannot push " << data << "." << endl;
-    }
-
-    cout << "Popping data from stack:" << endl;
-    while (true)
-    {
-        ret = s1.Pop_data();
-        if (ret == -1)
+        if (stack.Push_data(i))
         {
-            cout << "Stack is empty." << endl;
-            break;
+            cout << "Pushed " << i << " onto the stack." << endl;
         }
         else
         {
-            cout << ret << " popped from stack." << endl;
+            cout << "Stack is full. Cannot push " << i << "." << endl;
+        }
+    }
+    // Pop some data from the stack
+    for (int i = 1; i <= 6; i++)
+    {
+        int data = stack.Pop_data();
+        if (data != -1)
+        {
+            cout << "Popped " << data << " from the stack." << endl;
+        }
+        else
+        {
+            cout << "Stack is empty. Cannot pop." << endl;
         }
     }
     return 0;
